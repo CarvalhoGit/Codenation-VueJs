06c1b5844024ab4914a45cc5c70668322467f09c
Luis Carvalho <luiscvlh11@gmail.com>
2019-08-10 11:57:21 -0300

    Matutino terceira semana

---
+++ index.js
@@ -0,0 +1,56 @@
+// let date = new Date()
+// console.log(date.getHours(),":",date.getMinutes())
+
+// let numero = 10
+// console.log(numero % 2 == 0)
+
+// let nome = 'Eu'
+// let sobrenome = 'Eu de novo'
+// let nomecompleto = `${nome} lalal ${sobrenome}`
+// console.log(nomecompleto)
+
+// console.log(!!'teste')
+
+// function teste () {}
+// var teste = function () {};
+// var teste = () >= {};
+
+// function teste(){
+// 	this.teste = 0;
+// 	console.log(this.teste);
+// 	this.funcao = () => {
+// 		console.log(this.teste); // heranca, esse this é o mesmo this
+// 								// do pai
+// 	}
+// }
+
+// function teste ({ sobrenome }){
+// 	console.log(sobrenome);
+// }
+
+// function teste(valor){
+// 	valor = valor || 'teste'; // default 'teste'
+// 	console.log(valor);
+// }
+
+// teste();
+
+// let sobrenome = {'sobrenome' : 'Carvalho'}
+// teste(sobrenome);
+
+// let numero = 11;
+
+// if (numero === 10) {	
+// 	console.log("É 10")	
+// } else{
+// 	console.log("Não é 10")
+// }
+
+// if (typeof teste === 'number'){
+// 	console.log('OI')
+// } else if (typeof teste === 'string') {
+// 	console.log('Tchau')
+// } else {
+// 	console.log('Adeus')
+// }
+
