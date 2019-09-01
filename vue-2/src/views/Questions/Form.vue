74060491ab9f9934a498e5e47b5b59f94a33e3c2
Luis Fernando Carvalho <luiscvlh11@gmail.com>
2019-08-31 23:59:46 -0300

    Caguei

--- vue-2/src/views/Questions/Form.vue
+++ vue-2/src/views/Questions/Form.vue
@@ -1,95 +1,131 @@
 <template>
   <div>
-    <template v-for="(question, index_q) in questions">
-      <div class="div-question" :key="index_q">
+    <template v-for="(question, index_question
+    ) in questions">
+      <div class="div-question" :key="index_question
+      ">
         <h2 class="subtitle">{{question.pergunta}}</h2>
       </div>
       <hr />
-      <div class="field" v-for="(opcao, index_p) in question.opcoes">
-        <div class="control">
+      <div :key="index_question" class="field" data-test="pergunta">
+        <div
+          class="control"
+          v-for="opcao in question.opcoes"
+          :data-resposta="{'correta': opcao.correct === true, 'errada': opcao.correct === false}"
+        >
           <input
             class="input"
             :class="{'is-success': opcao.correct === true, 'is-danger': opcao.correct === false}"
             type="button"
             :value="opcao.data"
-            :key="index_p"
-            @click="isCorrect(index_q, index_p, question.resposta, opcao)"
+            @click="isCorrect(index_question
+            , opcao, question.resposta)"
+            data-test="opcao"
           />
         </div>
       </div>
       <hr />
     </template>
+    <article class="message is-success level" v-show="isDone">
+      <div class="message-header">
+        <p>Resultado</p>
+      </div>
+      <div class="message-body" :data-resultado="acertos">
+        Você acertou
+        <strong>{{ acertos }}</strong> de
+        <strong>{{ total_perguntas }}</strong> perguntas!
+      </div>
+      <div class="level-right">
+        <div class="level-item">
+          <button class="button is-danger" @click="resetQuiz" data-test="refazer">Refazer Quiz!</button>
+        </div>
+      </div>
+      <hr />
+    </article>
   </div>
-</template>
-    
 </template>
 
 <script>
 export default {
   data() {
     return {
+      data_correct: Boolean,
+      total_perguntas: 0,
+      acertos: 0,
+      respondidas: [],
       questions: [
         {
           pergunta: "Em quanto tempo de jogo nasce o arongueijo?",
           opcoes: [
-            { data: "2:20 min", correct: Boolean },
-            { data: "3:20 min", correct: Boolean },
-            { data: "4:20 min", correct: Boolean },
-            { data: "3:15 min", correct: Boolean }
+            { data: "2:20 min", correct: this.data_correct },
+            { data: "3:20 min", correct: this.data_correct },
+            { data: "4:20 min", correct: this.data_correct },
+            { data: "3:15 min", correct: this.data_correct }
           ],
-          resposta: 3
+          resposta: "3:15 min"
         },
         {
           pergunta: "Atualmente, qual é o dragão que possui o buff mais forte?",
           opcoes: [
-            { data: "Infernal", correct: Boolean },
-            { data: "Água", correct: Boolean },
-            { data: "Vento", correct: Boolean },
-            { data: "Montanha", correct: Boolean }
+            { data: "Infernal", correct: this.data_correct },
+            { data: "Água", correct: this.data_correct },
+            { data: "Vento", correct: this.data_correct },
+            { data: "Montanha", correct: this.data_correct }
           ],
-          resposta: 2
+          resposta: "Vento"
         },
         {
           pergunta: "Em quanto tempo de jogo as barricadas caem?",
           opcoes: [
-            { data: "14:00 min", correct: Boolean },
-            { data: "14:20 min", correct: Boolean },
-            { data: "14:40 min", correct: Boolean },
-            { data: "15:00 min", correct: Boolean }
+            { data: "14:00 min", correct: this.data_correct },
+            { data: "14:20 min", correct: this.data_correct },
+            { data: "14:40 min", correct: this.data_correct },
+            { data: "15:00 min", correct: this.data_correct }
           ],
-          resposta: 0
+          resposta: "14:00 min"
         },
         {
           pergunta: "Qual foi o último campeão lançado pela riot?",
           opcoes: [
-            { data: "Zoe", correct: Boolean },
-            { data: "Yuumi", correct: Boolean },
-            { data: "Pantheon", correct: Boolean },
-            { data: "Qiyana", correct: Boolean }
+            { data: "Zoe", correct: this.data_correct },
+            { data: "Yuumi", correct: this.data_correct },
+            { data: "Pantheon", correct: this.data_correct },
+            { data: "Qiyana", correct: this.data_correct }
           ],
-          resposta: 3
+          resposta: "Qiyana"
         },
         {
           pergunta: "O que é Riot Points?",
           opcoes: [
-            { data: "Pontuação de Rankeada", correct: Boolean },
-            { data: "Moeda virtual paga", correct: Boolean },
-            { data: "Moeda virtual grátis", correct: Boolean },
-            { data: "Experiência de nível", correct: Boolean }
+            { data: "Pontuação de Rankeada", correct: this.data_correct },
+            { data: "Moeda virtual paga", correct: this.data_correct },
+            { data: "Moeda virtual grátis", correct: this.data_correct },
+            { data: "Experiência de nível", correct: this.data_correct }
           ],
-          resposta: 1
+          resposta: "Moeda virtual paga"
         }
       ]
     };
   },
+  created() {
+    this.total_perguntas = this.questions.length;
+  },
+  computed: {
+    isDone() {
+      return this.respondidas.length === this.total_perguntas ? true : false;
+    }
+  },
   methods: {
-    isCorrect(index_q, index_p, resposta, opcao) {
-      let total_perguntas = this.questions.length;
-      let respondidas = [];
-      if (!(respondidas.indexOf(index_q) > -1)) {
-        index_p === resposta ? (opcao.correct = true) : (opcao.correct = false);
-        respondidas.push(index_q);
-        console.log(respondidas);
+    resetQuiz() {
+      location.reload();
+    },
+
+    isCorrect(index_question, opcao, resposta) {
+      if (!this.respondidas.includes(index_question)) {
+        opcao.data === resposta
+          ? ((opcao.correct = true), this.acertos++)
+          : (opcao.correct = false);
+        this.respondidas.push(index_question);
       }
     }
   }
